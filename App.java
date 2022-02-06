f741ac76aa4a4b6a0c78827cc3efd202adb5615b
therealmrwill <wru5006@psu.edu>
2022-02-06 04:44:19 -0500

    Redo/Fix of the wordSolver.runDataTest() Method

--- App.java
+++ App.java
@@ -17,53 +17,17 @@ public class App {
        
         WordSolver wordSolver = new WordSolver(baseFileString);
 
+        //TODO: If score array is updated re-run the data sorted
+
         //Enter a word and the program will figure out if it is attainable or not
-        //I don't think it runs fully at the moment though
-        System.out.println(wordSolver.wordFindable("ROCKS"));
+        wordSolver.wordFindable("Word here");
 
         System.out.println("Completed!");
         
         
     }
 
-    private static void debugFilePrinter(List<String> orderedValidWordMap, String scoreOrderedFile) {
-        PrintWriter pw = null;
-            final int LINE_LENGTH = 20;
-            int posInLine = 0;
     
-            try {
-                pw = new PrintWriter(scoreOrderedFile);
-    
-                for(int i = orderedValidWordMap.size() - 1; i >= 0; i--){
-                    if(posInLine % LINE_LENGTH == 0 && posInLine != 0){
-                        pw.println("");
-                        posInLine++;
-                    }
-                    else{
-                        posInLine++;
-                    }
-    
-                    if(orderedValidWordMap.get(i) != null){
-                        pw.print(orderedValidWordMap.get(i) + "  ");
-                    }
-                    else{
-                        pw.print("*Error*");
-                    }
-    
-                }
-    
-            } catch (Exception e) {
-                System.err.println("Uh Oh");
-                e.printStackTrace();
-            } finally{
-                if(pw != null){
-                    pw.close();
-                }   
-            }
-
-
-
-    }
 
 }
 
